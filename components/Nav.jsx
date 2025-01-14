d1810c7f69b6b0dc9e85fb59cee50d8f21571598
ANKIT KUMAR MISHRA <ankit.mishra@cloudkaptan.com>
2025-01-14 14:57:20 +0530

    feat:(main) developed nav bar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -11,6 +11,8 @@ const Nav = () => {
     const isUserLoggedIn = true;
 
     const [providers,setproviders] = useState(null);
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+
     useEffect(() => {
       const setProviders = async () => {
         const response = await getProviders();
@@ -51,6 +53,47 @@ const Nav = () => {
                     </Link>
                 </div>
             ) : (
+                <>
+                    {providers && Object.values(providers).map((provider) => (
+                        <button 
+                        type = "button" 
+                        key={provider.name}
+                        onClick ={() => signIn(provider.Id)}
+                        className="black_btn">
+                            Sign In 
+                        </button>
+                    ))}
+                </>
+            )}
+        </div>
+
+        {/* Mobile Navigation */}
+        <div className="sm:hidden flex relative">
+            {isUserLoggedIn ? (
+                <div className="flex">
+                    <Image 
+                        src = "assets/images/logo.svg"
+                        width = {37}
+                        height={37}
+                        className = "rounded-full"
+                        alt="profile"
+                        onClick ={() => setToggleDropdown((prev) => !prev)}
+                    />
+                        {toggleDropdown && (
+                            <div className="dropdown">
+                                <Link href="/profile" className="dropdown_link" onClick={() => setToggleDropdown(false)}>
+                                    My Profile
+                                </Link>
+                                <Link href="/create-prompt" className="dropdown_link" onClick={() => setToggleDropdown(false)}>
+                                    Create Prompt
+                                </Link>
+                                <button type="button" onClick ={() => {setToggleDropdown(false); signOut();}} className="mt-5 w-full black_btn">
+                                    Sign Out
+                                </button>
+                            </div>
+                        )}
+                </div>
+            ):(
                 <>
                     {providers && Object.values(providers).map((provider) => (
                         <button 
